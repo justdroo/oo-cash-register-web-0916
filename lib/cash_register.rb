57e001dfbcc4bac04c567bbc5c55989e2f8a9a2a
Flatiron School <flatironschool@Drew-2.local>
2016-09-29 17:03:30 -0400

    Complete

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+class CashRegister
 
+ attr_accessor :total
+ attr_reader :discount
+
+ def initialize (discount = 0)
+   @total = 0
+   @discount = discount
+   @items_list = []
+   @last_transaction = []
+ end
+
+ def add_item(title, price, quantity = 1)
+   @total = @total + (price*quantity)
+   quantity.times do
+     @items_list << title
+   end
+
+   @last_transaction = [title, price, quantity]
+ end
+
+ def apply_discount
+
+   if @discount == 0
+     "There is no discount to apply."
+   else
+     @total = ((100-@discount)*@total)/100
+     "After the discount, the total comes to $#{@total}."
+   end
+
+ end
+
+ def items
+   @items_list
+ end
+
+ def void_last_transaction
+
+   @total = @total - (@last_transaction[1] * @last_transaction[2])
+
+   @last_transaction[2].times do
+     @items_list.pop
+   end
+
+ end
+
+end
